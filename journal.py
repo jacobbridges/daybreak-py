0489098655d10ea7f6909c3ec97cad2214b2af1f
jacobbridges <him@jacobandkate143.com>
2015-06-12 00:05:47 -0400

    ported some functions over..not yet complete

--- journal.py
+++ journal.py
@@ -1,39 +1,66 @@
 __author__ = 'stanley'
 
-from Queue import *
+import os
+from .FileLock import FileLock
+from Queue import Queue
+from threading import Thread
+
 
 class Journal(Queue):
 
-    def __init__(self, file, format, serializer, block):
-        self.size
+    def __init__(self, file_path, storage_format, serializer, block):
+        """
+        :param filepath: Path to journal file
+        :param storage_format: Format to save the db contents to the journal file
+        :param serializer:
+        :param block:
+        :return:
+        """
 
-        self.file=file
-        self.format=format
-        self.serializer=serializer
-        self.emit=block
+        # Call to super class's constructor
+        Queue.__init__(self)
 
+        self.file_path = file_path
+        self.format = storage_format
+        self.serializer = serializer
+        self.emit = block
+
+        self.file_lock = FileLock(file_path)
         self.open()
 
-        self.worker = Thr
+        self.worker = Thread(name='journal_worker', target=self.worker)
+        self.load()
         pass
 
     def size(self):
-        pass
+        return self.qsize()
 
     def closed(self):
-        pass
+        return self.file.closed
 
     def close(self):
-        pass
+        with self.mutex:
+            self.queue.clear()
+        self.worker.join()
+        self.file.close()
 
     def load(self):
-        pass
+        self.file.flush()
+        self.join()
+        self.replay()
 
     def lock(self):
-        pass
+        self.flush()
+        with self.file_lock:
+            self.replay()
+            self.flush()
 
     def clear(self):
-        pass
+        self.flush()
+
+    def flush(self):
+        self.join()
+        self.file.flush() if not self.closed else None
 
     def compact(self):
         pass
@@ -42,10 +69,16 @@ class Journal(Queue):
         pass
 
     def replay(self):
-        pass
+        buf = buffer(bytearray(self.file.readall()))
+        self.size += self.format.parse(buf, self.emit)
 
     def open(self):
-        pass
+        self.file.close() if self.file is not None else None
+        self.file = open(self.file_path, 'ab+')
+        stat = os.stat(self.file_path)
+        self.inode = stat.st_ino
+        self.file.write(self.format.header) if stat.st_size == 0 else None
+        self.pos = None
 
     def read(self):
         pass
